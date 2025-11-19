50c781418742bb9c3d83f7c55b10e24c3a13e90f
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-11-19 05:29:57 +0000

    borrador de sort y clase search

---
+++ ProyectoBanco/LogicaSistema/SearchService.java
@@ -0,0 +1,23 @@
+public class SearchService {
+    public int binarySearcById(List<Account> accounts, String id) {
+        if (accounts == null || id == null) {
+            return -1;
+        }
+        int low = 0, high = accounts.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            String midId = accounts.get(mid).getId();
+            int cmp = midId.compareTo(id);
+            if (cmp == 0) {
+                return mid;
+            }
+            if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            return -1;
+        }
+    }
+
+}
---
+++ ProyectoBanco/LogicaSistema/SortService.java
@@ -0,0 +1,14 @@
+public class SortService {
+    public void mergeSortByBalance(List<Account> accounts){
+        if (accounts == null || accounts.size() <= 1) {
+            return;
+        }
+    }
+
+    public void quickSortByName(List<Account> accounts){
+        if (accounts == null || accounts.size() <= 1) {
+            return;
+        } 
+    }
+}
+    
