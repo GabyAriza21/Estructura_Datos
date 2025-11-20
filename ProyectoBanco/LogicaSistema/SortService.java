e673735b5f3bcb2e56d031d22902872ecf204f8a
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-11-20 01:23:38 +0000

    comentarios de que hace cada linea respectivamente

--- ProyectoBanco/LogicaSistema/SortService.java
+++ ProyectoBanco/LogicaSistema/SortService.java
@@ -6,45 +6,45 @@ public class SortService {
         mergeSortByBalance(accounts, 0, accounts.size() - 1); //si tiene mas de un elemento, divide la lista en sublistas y luego les cambia el orden
     }
 
-    private void mergeSortByBalance(List<Account> accounts, int left, int right) {
-        if (left >= right) {
+    private void mergeSortByBalance(List<Account> accounts, int left, int right) { // ordena la sublista de cuentas
+        if (left >= right) { // recibe la lista de cuentas y los indices izquierdo y derecho
             return;
         }
 
-        int mid = (left + right) / 2;
+        int mid = (left + right) / 2; //verifica si el indice iz mayor= al derecho, si hay mas de un elemento, calcula el punto medio (/2)
 
-        mergeSortByBalance(accounts, left, mid);
-        mergeSortByBalance(accounts, mid + 1, right);
+        mergeSortByBalance(accounts, left, mid);// llama la funcion para organizar la mitad izquierda 
+        mergeSortByBalance(accounts, mid + 1, right); // llama la funcion para organizar la mitad derecha
 
-        mergeByBalance(accounts, left, mid, right);
+        mergeByBalance(accounts, left, mid, right); //pone en una sola sublista ordenada a las dos mitades 
     }
 
-    private void mergeByBalance(List<Account> accounts, int left, int mid, int right) {
-        List<Account> temp = new ArrayList<>();
-        int i = left;
-        int j = mid + 1;
+    private void mergeByBalance(List<Account> accounts, int left, int mid, int right) { //combina 2 sublistas ordenadas de cuentas en una sola lista ordenada por saldo
+        List<Account> temp = new ArrayList<>(); //se crea una lista temporal: guarda elementos combinados en orden
+        int i = left; //inicio de la primera mitad
+        int j = mid + 1; //inicio de la segunda mitad
 
-        while (i <= mid && j <= right) {
-            if (accounts.get(i).getBalance() <= accounts.get(j).getBalance()) {
-                temp.add(accounts.get(i));
-                i++;
-            } else {
-                temp.add(accounts.get(j));
-                j++;
+        while (i <= mid && j <= right) {//compara saldos de las cuentas en ambas mitades
+            if (accounts.get(i).getBalance() <= accounts.get(j).getBalance()) {//si el saldo1 es menor= al saldo2
+                temp.add(accounts.get(i));//esa cuenta se va para temp
+                i++;//incrementa i
+            } else {//si no, saldo2
+                temp.add(accounts.get(j));//va para temp
+                j++;//incrementa j
             }
         }
-        while (i <= mid) {
-            temp.add(accounts.get(i));
+        while (i <= mid) { //si la primera mitad tiene elementos sin procesar
+            temp.add(accounts.get(i));//se agregan a temp
             i++;
         }
 
-        while (j <= right) {
-            temp.add(accounts.get(j));
+        while (j <= right) {//si la segunda mitad tiene elementos sin procesar
+            temp.add(accounts.get(j));//se agregan a temp
             j++;
         }
 
-        for (int k = left; k <= right; k++) {
-            accounts.set(k, temp.get(k - left));
+        for (int k = left; k <= right; k++) { //copia los elementos ordenados de temp a la lista original con las posiciones que son
+            accounts.set(k, temp.get(k - left)); //cuadra el indice de temp con la posicion correcta de la lista original
         }
     }
 
