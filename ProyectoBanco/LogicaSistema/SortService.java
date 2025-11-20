9ae7534b13698d2b0353b36f274b4a18fd062902
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-11-20 02:15:26 +0000

    avance clase SortService

--- ProyectoBanco/LogicaSistema/SortService.java
+++ ProyectoBanco/LogicaSistema/SortService.java
@@ -49,8 +49,40 @@ public class SortService {
     }
 
     public void quickSortByName(List<Account> accounts) { // ordena cuentas por nombre
-        if (accounts == null || accounts.size() <= 1) {
+        if (accounts == null || accounts.size() <= 1) { //mira si la lista es nula o si tiene o no elementos
             return;
-        } // iniciar codigo busqueda por nombre
+        } quickSortByName(accounts, 0, accounts.size() -1); //si tiene mas de 1 elemento, divide la lista en sublistas y ordena cada parte segun el nombre del titular
+    }
+
+    private void quickSortByName(List<Account> accounts, int low, int high) { // ordena la sublista de cuentas por nombre del titular, recibe la llista de cuentas, y low-high indices
+        if (low < high) { //mira si el indice low es menor que high
+            int pivotIndex = partitionByName(accounts, low, high); //si cumple, llama a partitionByName: ordena la sublista 
+            quickSortByName(accounts, low, pivotIndex - 1); //los nombres menores o iguales al pivote quedan en iz, los nombres mayores al pivote quedan en derecha
+            quickSortByName(accounts, pivotIndex + 1, high); //ordena las sublistas a la izquierda y derecha del pivote
+        }  
+    }
+
+    private int partitionByName(List<Account> accounts, int low, int high){ //recibe la lista de cuentas, el indice inicial de la sublista que se va a particionar y el indice final de la sublista
+        String pivot = accounts.get(high).getHolderName().toLowerCase();//pivot: nombre del titular de la cuenta en la posicion high de lista-accounts y se convierte a minusculas
+        int i = low - 1; // marca l aposicion donde se colocan los elementos menores= al pivote. en el recorrido de la sublista, los que cumplen la condicion se pasan a la iz de i
+
+        for (int j = low; j < high; j++){ // recorre la sublista desde el indice low hasta high-1
+            String currentName = accounts.get(j).getHolderName().toLowerCase(); //se obtiene el nombre del titular de la cuenta en la posicion j y se convierte a minusculas
+
+            if (currentName.compareTo(pivot) <= 0) { //compara el nombre actual con el pivote
+                i++; 
+                swap(accounts, i, j);// si el nombre actual es menor= al pivote, incrementa i y intercambia las cuentas en las posiciones i y j
+            }
+        }
+
+        swap(accounts, i + 1, high);//se intercambia el elemento en la posicion i+1 con el pivote, dejando al pivote en la posicion que es
+        return i + 1; //retorna la nueva posicion del pivote
+
+    }
+
+    private void swap(List<Account> accounts, int i, int j){ //intercambia las cuentas en las posiciones i y j de la lista accounts
+        Account temp = accounts.get(i); // guarda la cuenta en la posicion i en una variable temporal
+        accounts.set(i, accounts.get(j)); // coloca la cuenta en la posicion j en la posicion i
+        accounts.set(j, temp);// coloca la cuenta guardada en temp en la posicion j (los elementos en i y j quedan intercambiados)
     }
 }
